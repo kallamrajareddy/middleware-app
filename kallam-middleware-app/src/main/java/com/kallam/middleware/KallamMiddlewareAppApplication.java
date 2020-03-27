7af64eca16381ee41e4354ee3c09426ebe9f4177
Kallam Rajendra Reddy <kallamrajareddy@gmail.com>
2020-03-27 15:38:00 +0530

    Middle Ware project
    Initial Setup

---
+++ kallam-middleware-app/src/main/java/com/kallam/middleware/KallamMiddlewareAppApplication.java
@@ -0,0 +1,13 @@
+package com.kallam.middleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KallamMiddlewareAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KallamMiddlewareAppApplication.class, args);
+	}
+
+}
---
+++ kallam-middleware-app/src/test/java/com/kallam/middleware/KallamMiddlewareAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kallam.middleware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KallamMiddlewareAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
